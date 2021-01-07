d5b5adef94519878daf538c2569add3825c26454
mrjlancaster <lancast001@gmail.com>
2021-01-06 21:48:30 -0600

    setup lighting

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 import { GLTFLoader } from 'https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js';
 
+// Scene setup
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xdddddd);
-
+// scene.background = new THREE.Color(0xdddddd);
 
 // Camera setup
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const fov = 75;
+// const aspect = 2
+const near = 0.1;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, near, far);
 camera.position.set(0, 2, 4);
-
-const ambient = new THREE.AmbientLight(0x404040, 10);
-scene.add(ambient);
 
 // Renderer setup
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
+// Light
+const ambientLight = new THREE.AmbientLight(0x404040, 15);
+scene.add(ambientLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+directionalLight.position.set(0, 1, 0);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+
 // // load 3d model
 let loader = new GLTFLoader();
-loader.load('./model.glb', (gltf) => {
+loader.load('./bottle-model.glb', (gltf) => {
     scene.add(gltf.scene);
 })
 
