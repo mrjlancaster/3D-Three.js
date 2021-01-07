8dbbbb5670351b88b4282cff4bfac0094f2590ee
mrjlancaster <lancast001@gmail.com>
2021-01-06 22:42:58 -0600

    add point light

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ const scene = new THREE.Scene();
 
 // Camera setup
 const fov = 75;
-// const aspect = 2
+const aspect = 2
 const near = 0.1;
 const far = 1000;
-const camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, near, far);
-camera.position.set(0, 3, 5);
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.set(0.4, 3, 5);
 
 // Renderer setup
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
@@ -21,12 +21,16 @@ renderer.setClearColor('#ecf0f1');
 renderer.setPixelRatio(window.devicePixelRatio);
 
 // Lighting
-const ambientLight = new THREE.AmbientLight(0x404040, 18);
+const ambientLight = new THREE.AmbientLight(0x404040, 20);
 scene.add(ambientLight);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
 directionalLight.position.set(0, 1, 0);
 directionalLight.castShadow = true;
 scene.add(directionalLight);
+
+const pointLight = new THREE.PointLight( 0xff0000, 1, 100 );
+pointLight.position.set( 0, 300, 500 );
+scene.add(pointLight);
 
 // load 3d model
 let loader = new GLTFLoader();
@@ -34,25 +38,25 @@ loader.load('./bottle-model.glb', (gltf) => {
     scene.add(gltf.scene);
 })
 
+
 // Control setting
 const controls = new OrbitControls( camera, renderer.domElement );
-console.log(controls);
 
 // insert all to HTML document
 document.body.appendChild(renderer.domElement);
 
 // animation
 const animate = () => {
-    renderer.render(scene, camera);
     requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
 
 // Handle screen resize
 const onWindowResize = () => {
     camera.aspect = window.clientWidth / window.clientHeight;
     camera.updateProjectionMatrix(window.clientWidth, window.clientHeight);
-    renderer.setSize()
+    renderer.setSize(window.clientWidth, window.clientHeight)
 }
 
-animate();
 window.addEventListener('resize', onWindowResize);
+animate();
