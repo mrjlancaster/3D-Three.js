bc46402dd6b3d2b2eb450e4200c29092858c53d8
mrjlancaster <lancast001@gmail.com>
2021-01-08 21:44:53 -0600

    create spot light shadow

--- main.js
+++ main.js
@@ -4,52 +4,81 @@ import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/Orb
 
 // Scene setup
 const scene = new THREE.Scene();
-// scene.background = new THREE.Color(0xdddddd);
+scene.background = new THREE.Color(0xdddddd);
+
+scene.add(new THREE.AxesHelper(500))
 
 // Camera setup
-const fov = 75;
+const fov = 90;
 const aspect = 2;
 const near = 0.1;
 const far = 1000;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(0.2, 3, 3); // x, y, z
+camera.position.set(0, 2, 4); // x, y, z
  
 // Renderer setup
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor('#ecf0f1'); // background/scene color
+// renderer.setClearColor('#ecf0f1'); // background/scene color
 renderer.setPixelRatio(window.devicePixelRatio);
 
 // Lighting
-const ambientLight = new THREE.AmbientLight(0x404040, 13);
-scene.add(ambientLight);
+// const ambientLight = new THREE.AmbientLight(0x404040, 1);
+// scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0);
-directionalLight.position.set(0, 1, 0);
-directionalLight.castShadow = true;
-scene.add(directionalLight);
+const hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
+scene.add(hemiLight);
 
-const pointLightOne = new THREE.PointLight(0xc4c4c4, 5);
-pointLightOne.position.set( 0, 100, 500 );
-scene.add(pointLightOne);
+renderer.toneMapping = THREE.CineonToneMapping;
+renderer.shadowMap.enabled = true;
+const spotLight = new THREE.SpotLight(0xffa95c, 4);
+spotLight.castShadow = true;
+spotLight.shadow.bias = -0.0001;
+spotLight.shadow.mapSize.width = 1024 * 4;
+spotLight.shadow.mapSize.height = 1024 * 4;
+scene.add(spotLight);
 
-const pointLightTwo = new THREE.PointLight(0xc4c4c4, 5);
-pointLightTwo.position.set(500,100,0);
-scene.add(pointLightTwo);
+// const directionalLight = new THREE.DirectionalLight(0xffffff, 0);
+// directionalLight.position.set(0, 1, 0);
+// directionalLight.castShadow = true;
+// scene.add(directionalLight);
 
-const pointLightThree = new THREE.PointLight(0xc4c4c4, 5);
-pointLightThree.position.set(0,100,-500);
-scene.add(pointLightThree);
+// const pointLightOne = new THREE.PointLight(0xc4c4c4, 5);
+// pointLightOne.position.set( 0, 100, 500 );
+// scene.add(pointLightOne);
 
-const pointLightFour = new THREE.PointLight(0xc4c4c4, 5);
-pointLightFour.position.set(-500,300,500);
-scene.add(pointLightFour);
+// const pointLightTwo = new THREE.PointLight(0xc4c4c4, 5);
+// pointLightTwo.position.set(500,100,0);
+// scene.add(pointLightTwo);
 
-// load 3d model
+// const pointLightThree = new THREE.PointLight(0xc4c4c4, 5);
+// pointLightThree.position.set(0,100,-500);
+// scene.add(pointLightThree);
+
+// const pointLightFour = new THREE.PointLight(0xc4c4c4, 5);
+// pointLightFour.position.set(-500,300,500);
+// scene.add(pointLightFour);
+
+// MODEL SETTINGS
 let loader = new GLTFLoader();
-loader.load('./Clearex-500-embedded.gltf', (gltf) => {
+loader.load('complete_model.glb', (gltf) => {
+    const model = gltf.scene;
 
-    scene.add(gltf.scene);
+    // Positioning model on screen
+    model.position.set(0, -1, 0); // x, y, z
+
+    model.traverse(n => {
+        if (n.isMesh) {
+            n.castShadow = true;
+            n.receiveShadow = true;
+            if (n.material.map) n.material.map.anisotropy = 16;
+        }
+    })
+
+    console.log(model);
+
+    // scene.add(gltf.scene);
+    scene.add(model)
 })
 
 
@@ -68,6 +97,11 @@ document.body.appendChild(renderer.domElement);
 const animate = () => {
     requestAnimationFrame(animate);
     controls.update();
+    spotLight.position.set(
+        camera.position.x + 10,
+        camera.position.y + 10,
+        camera.position.z + 10
+    )
     renderer.render(scene, camera);
 }
 
